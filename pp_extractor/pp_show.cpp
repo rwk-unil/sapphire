0b73f1e3d3a5eb9102df8ca3ca5954b9dc5d1257
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2023-03-10 16:56:32 +0100

    Added VCFotographer command generation to pp_show

--- pp_extractor/pp_show.cpp
+++ pp_extractor/pp_show.cpp
@@ -22,10 +22,12 @@ int main(int argc, char**argv) {
     app.add_option("-f,--vcf-file", filename, "Input VCF file name (variants only)");
     std::string bfname = "-";
     app.add_option("-b,--bin-file", bfname, "Binary file name");
-    //std::string ofname = "-";
-    //app.add_option("-o,--output", ofname, "Output file name");
+    std::string ofname = "-";
+    app.add_option("-o,--output", ofname, "Output file name");
     size_t sample = -1;
     app.add_option("-s,--sample", sample, "Sample index");
+    bool igv = false;
+    app.add_flag("--igv", igv, "Outputs IGV batch file (requires output file name)");
 
     CLI11_PARSE(app, argc, argv);
 
@@ -44,6 +46,11 @@ int main(int argc, char**argv) {
         exit(app.exit(CLI::CallForHelp()));
     }
 
+    if (igv && ofname.compare("-") == 0) {
+        std::cerr << "Requires output filename\n";
+        exit(app.exit(CLI::CallForHelp()));
+    }
+
     std::cout << "Loading variants ..." << std::endl;
     VarInfoLoader vars(filename);
     std::cout << "Num VCF lines : " << vars.vars.size() << std::endl;
@@ -58,10 +65,35 @@ int main(int argc, char**argv) {
     //himm.print_positions(sample);
     std::vector<HetInfo> v;
     himm.fill_het_info(v, sample);
-    std::cout << "---" << std::endl;
-    for (auto hi : v) {
-        std::cout << vars.vars[hi.vcf_line].to_string() << "\t";
-        std::cout << hi.to_string() << std::endl;
+
+    if (igv) {
+        std::ofstream ofs(ofname);
+        if (!ofs.is_open()) {
+            std::cerr << "Could not open " << ofname << std::endl;
+            exit(-1);
+        }
+
+        // https://software.broadinstitute.org/software/igv/batch
+        ofs << "new" << std::endl;
+        ofs << "genome hg38 1k/GATK" << std::endl;
+        //ofs << "load xxxxxx.cram" << std::endl;
+        //ofs << "load TODO VCF" << std::endl;
+        ofs << "viewaspairs" << std::endl;
+        ofs << "snapshotDirectory ~/snap" << std::endl;
+        for (auto hi : v) {
+            ofs << vars.vars[hi.vcf_line].to_vcfotographer_string() << std::endl;
+            ofs << "snapshot" << std::endl;
+        }
+        ofs.flush();
+        ofs.close();
+
+        std::cout << "Successfully wrote the file " << ofname << std::endl;
+    } else {
+        std::cout << "---" << std::endl;
+        for (auto hi : v) {
+            std::cout << vars.vars[hi.vcf_line].to_string() << "\t";
+            std::cout << hi.to_string() << std::endl;
+        }
     }
 
     return 0;
