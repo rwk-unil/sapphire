8efaf4e0fbaa897b45352c8957df4d259e160ec8
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2023-04-11 16:13:37 +0200

    Added script to run bin merger applet

--- dnanexus_app/scripts/run_bin_merger.sh
+++ dnanexus_app/scripts/run_bin_merger.sh
@@ -9,15 +9,13 @@ fi
 
 # Get the path of this script
 SCRIPTPATH=$(realpath  $(dirname "$0"))
-
-INSTANCE="mem2_ssd1_v2_x2"
-BIN_ID=""
-OFNAME=""
-SPLIT_SIZE="1000"
 DESTINATION="/"
+INSTANCE="mem2_ssd1_v2_x2"
 
 # Source common variables and functions
 source "${SCRIPTPATH}/common.sh"
+
+BIN_FILE_DIR=""
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -27,18 +25,8 @@ key="$1"
 # Command line argument parsing from :
 # https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 case $key in
-    -b|--bin-id)
-    BIN_ID="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -o|--output)
-    OFNAME="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -n|--split-size)
-    SPLIT_SIZE="$2"
+    --bin-dir)
+    BIN_DIR="$2"
     shift # past argument
     shift # past value
     ;;
@@ -50,28 +38,26 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-if [ -z "${BIN_ID}" ]
+if [ -z "${BIN_DIR}" ]
 then
-    echo "Specify an input binary file ID with --bin-id <actual_input_id>"
-    echo "This file must be generated from the VCF passed"
+    echo "Specify an input binary file directory with --bin-dir <actual_input_dir>"
     exit 1
 fi
 
-BIN_FILENAME=$(dx_id_to_path "${BIN_ID}")
-echo "BIN FILENAME    = ${BIN_FILENAME}"
-
-if [ -z "${OFNAME}" ]
-then
-    OFNAME=$(basename "${BIN_FILENAME}")
-fi
-
-command=(dx run applets/pp-toolkit/bin-splitter-applet \
-    -ibinary_file_to_split=${BIN_ID} -isplit_size=${SPLIT_SIZE} \
+command=(dx run \
     --destination "${DESTINATION}" \
     ${COST_LIMIT_ARG} --instance-type ${INSTANCE} -y \
-    --name "PP-Toolkit : Step 2 - Bin splitter" \
-    --tag "split")
+    --name "PP-Toolkit : Step 4 - Bin merger" \
+    --tag "merge" \
+    applets/pp-toolkit/bin-merger-applet)
 
+for split_bin_file in $(dx ls ${BIN_DIR})
+do
+    echo Found file "${split_bin_file}"
+    local_id=$(dx describe "${BIN_DIR}/${split_bin_file}" --json | jq -r '.id')
+    echo With ID : "${local_id}"
+    command+=("-isplitted_binary_files=${local_id}")
+done
 
 echo "${command[@]}"
 
