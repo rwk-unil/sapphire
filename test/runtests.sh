102fc73cf3c582ffa8916d7ef284316894c6f144
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2023-04-13 10:26:46 +0200

    [Major] Added test suite

---
+++ test/runtests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! command -v realpath &> /dev/null
+then
+    realpath() {
+        [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+    }
+fi
+
+# Get the path of this script
+SCRIPTPATH=$(realpath  $(dirname "$0"))
+
+cd "${SCRIPTPATH}"
+
+./cukinia/cukinia tests.conf
---
+++ test/scripts/test_pp_extractor.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+if ! command -v realpath &> /dev/null
+then
+    realpath() {
+        [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+    }
+fi
+
+# Get the path of this script
+SCRIPTPATH=$(realpath  $(dirname "$0"))
+
+FILENAME=""
+REFERENCE=""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+# Command line argument parsing from :
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+case $key in
+    -f|--filename)
+    FILENAME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -r|--reference)
+    REFERENCE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ -z "${FILENAME}" ]
+then
+    echo "Specify a filename with --filename, -f <filename>"
+    exit 1
+fi
+
+if [ -z "${REFERENCE}" ]
+then
+    echo "Specify a filename with --reference, -r <filename>"
+    exit 1
+fi
+
+echo "FILENAME        = ${FILENAME}"
+echo "REFERENCE       = ${REFERENCE}"
+
+TMPDIR=$(mktemp -d -t pp_XXXXXX) || { echo "Failed to create temporary directory"; exit 1; }
+
+echo "Temporary directory : ${TMPDIR}"
+
+function exit_fail_rm_tmp {
+    echo "Removing directory : ${TMPDIR}"
+    rm -r ${TMPDIR}
+    exit 1
+}
+
+OUTPUTNAME="$(basename "${FILENAME}")".bin
+
+"${SCRIPTPATH}"/../../pp_extractor/pp_extract -f "${FILENAME}" -o ${TMPDIR}/"${OUTPUTNAME}" || { echo "Failed to extract ${FILENAME}"; exit_fail_rm_tmp; }
+cmp "${REFERENCE}" ${TMPDIR}/"${OUTPUTNAME}" || { echo "[KO] Output file and reference are different"; exit_fail_rm_tmp; }
+
+echo "[OK] The extracted file and reference are the same"
+
+rm -r $TMPDIR
+exit 0
