75368ab8d68cfa249d84e17bcc7b2a060942a893
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2023-04-14 08:39:12 +0200

    Added reading more info from file in sample info loader

--- include/sample_info.hpp
+++ include/sample_info.hpp
@@ -4,15 +4,61 @@
 #include <fstream>
 #include <string>
 
+class SampleInfo {
+public:
+    SampleInfo(size_t index, const std::string& name) :
+        original_index(index),
+        name(name),
+        cram_file_path("")
+    {}
+
+    SampleInfo(size_t index, const std::string& name, const std::string& cram_file_path) :
+        original_index(index),
+        name(name),
+        cram_file_path(cram_file_path)
+    {}
+
+    size_t original_index = -1;
+    std::string name;
+    std::string cram_file_path;
+};
+
+std::vector<std::string> tokenize(const std::string& s, const std::string& delim) {
+    size_t pos_start = 0, pos_end, delim_len = delim.length();
+    std::string token;
+    std::vector<std::string> result;
+
+    while ((pos_end = s.find(delim, pos_start)) != std::string::npos) {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        result.push_back(token);
+    }
+
+    return result;
+}
+
 class SampleInfoLoader {
 public:
     SampleInfoLoader(const std::string& sample_filename) {
         if (sample_filename.compare("-") == 0) return;
         std::ifstream file(sample_filename);
         std::string line;
+        size_t i = 0;
         while (std::getline(file, line)) {
             if (line.length()) {
-                sample_names.push_back(line);
+                auto tokens = tokenize(line, ",");
+                if (tokens.size() == 1) {
+                    sample_names.push_back(line); // Same as tokens [0]
+                    samples.push_back(SampleInfo(i, line));
+                } else if (tokens.size() > 1) {
+                    sample_names.push_back(tokens[1]);
+                    const std::string& cram_path = (tokens.size() > 2) ? tokens[2] : std::string("");
+                    samples.push_back(SampleInfo(std::stoi(tokens[0]), tokens[1], cram_path));
+                } else {
+                    std::cerr << "Could not load line : '" << line << "' from file" << sample_filename << std::endl;
+                    /// @todo throw
+                }
+                i++;
             }
         }
     }
@@ -36,6 +82,7 @@ public:
     }
 
     std::vector<std::string> sample_names;
+    std::vector<SampleInfo> samples;
 };
 
 #endif /* __SAMPLE_INFO_HPP__ */
